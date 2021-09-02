8a85720fc62f91a4d89eb3892453fdce777b2c7f
Ish J <ish@knights.ucf.edu>
2021-09-01 23:55:10 -0700

    scrimba for today

---
+++ Extras/Extra2/script.js
@@ -0,0 +1,32 @@
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+
+let firstCard = 7
+let secondCard = 5
+
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log("Would you like to draw a new card?")
+} else if ( sum === 21 ) {
+    console.log("Big money you won!!")
+} else {
+    console.log("Sorry you're a loser")
+}
+
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+let age = 22
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+if ( age < 21) {
+    console.log("Cant get in this club homeboy/homeigirl")
+} else {
+    console.log("Time to get turnt")
+}
