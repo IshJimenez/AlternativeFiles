ec4e7cb20dd05175280a6fe9ee9ddcae70c43a38
Ish J <ish@knights.ucf.edu>
2021-09-02 23:28:46 -0700

    scrimba for today

--- Extras/Extra2/script.js
+++ Extras/Extra2/script.js
@@ -30,3 +30,37 @@ if ( age < 21) {
 } else {
     console.log("Time to get turnt")
 }
+
+// Check if the person is elegible for a birthday card from the King! (100)
+
+let atge = 100
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+if (atge < 100) {
+    console.log("Not elegible")
+} else if ( atge === 100 ) {
+    console.log("Here is your birthday card from the King!")
+} else {
+    console.log("Not elegible, you have already gotten one")
+}
+
+let firstC= 10
+let secondC= 7
+let summ = firstC + secondC
+
+// Write the conditional according to these rules:
+
+// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
+// else if exactly 21 -> ""Big money you won!!" 🥳"
+// else -> "You're out of the game! 😭"
+
+if (summ <= 20) {
+    console.log("Would you like to draw a new card?")
+} else if ( sum === 21 ) {
+    console.log("Sorry you're a loser")
+} else {
+    console.log("Sorry you're a loser")
+}
